fb68fb6d0902550bd3820840af39a43047269063
KamilaRys <kamila.joanna.rys@gmail.comgit config -lgit config --global user.email kamila.joanna.rys@gmail.com>
2022-03-25 12:30:44 +0100

    added getAllCoachesTest()

--- src/test/java/com/jwzp_kr_kj/controllers/CoachControllerTest.java
+++ src/test/java/com/jwzp_kr_kj/controllers/CoachControllerTest.java
@@ -3,14 +3,19 @@ package com.jwzp_kr_kj.controllers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.jwzp_kr_kj.core.Coach;
 import com.jwzp_kr_kj.services.CoachService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
+@ExtendWith(MockitoExtension.class)
 public class CoachControllerTest {
 
     @Mock
@@ -19,9 +24,22 @@ public class CoachControllerTest {
 
     @Test
     public void getAllCoachesEmptyTest() throws JsonProcessingException {
-        var coaches = new CoachController(coachService);
-        var result = coaches.printCoaches();
+        Mockito.when(coachService.getAllCoaches()).thenReturn(Collections.emptyList());
+        var result = new CoachController(coachService).printCoaches();
         var expected = ResponseEntity.ok(ow.writeValueAsString(Collections.emptyList()));
+        assert result.equals(expected);
+        Mockito.verify(coachService, Mockito.times(1)).getAllCoaches();
+    }
+
+    @Test
+    public void getAllCoachesTest() throws JsonProcessingException {
+        List<Coach> coaches = List.of(
+                new Coach(0, "Ygrek", "Iksinski", 1990),
+                new Coach(1, "Ktos", "Fajny", 1993)
+        );
+        Mockito.when(coachService.getAllCoaches()).thenReturn(coaches);
+        var result = new CoachController(coachService).printCoaches();
+        var expected = ResponseEntity.ok(ow.writeValueAsString(coaches));
         assert result.equals(expected);
     }
 }
