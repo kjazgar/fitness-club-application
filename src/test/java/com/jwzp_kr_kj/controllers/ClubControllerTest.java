f092b81d6ff05f980dd86b39ca899135ea2fd83a
KamilaRys <kamila.joanna.rys@gmail.comgit config -lgit config --global user.email kamila.joanna.rys@gmail.com>
2022-03-25 14:30:54 +0100

    added annotation - nothing special

--- src/test/java/com/jwzp_kr_kj/controllers/ClubControllerTest.java
+++ src/test/java/com/jwzp_kr_kj/controllers/ClubControllerTest.java
@@ -5,11 +5,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.jwzp_kr_kj.services.ClubService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Collections;
 
+@ExtendWith(MockitoExtension.class)
 public class ClubControllerTest {
 
     @Mock
