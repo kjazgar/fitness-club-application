7e7d2ea516a5611568810d40c108f3b926e92278
kamila <kamila.rys@student.uj.edu.pl>
2022-04-22 19:39:00 +0200

    fixed ClubControllerTest - added link

--- src/test/java/com/jwzp_kr_kj/controllers/ClubControllerTest.java
+++ src/test/java/com/jwzp_kr_kj/controllers/ClubControllerTest.java
@@ -3,14 +3,20 @@ package com.jwzp_kr_kj.controllers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.jwzp_kr_kj.models.records.ClubRecord;
 import com.jwzp_kr_kj.services.ClubService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Collections;
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @ExtendWith(MockitoExtension.class)
 public class ClubControllerTest {
@@ -21,9 +27,11 @@ public class ClubControllerTest {
 
     @Test
     public void getAllClubsEmptyTest() throws JsonProcessingException {
+        List<ClubRecord> emptyClubs = Collections.emptyList();
+        Mockito.when(clubService.getAllClubs()).thenReturn(emptyClubs);
+        var expected = ResponseEntity.ok((ow.writeValueAsString(CollectionModel.empty().add(linkTo(ClubController.class).withSelfRel()))));
         var clubs = new ClubController(clubService);
         var result = clubs.printClubs();
-        var expected = ResponseEntity.ok(ow.writeValueAsString(Collections.emptyList()));
         assert result.equals(expected);
     }
 }
